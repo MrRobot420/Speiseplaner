64a77e3613d48de783890c2ee7d875c839dc3dbf
MrRobot420 <max.karlchen@web.de>
2018-09-19 16:13:42 +0200

    * Added Transition from Main to Meallist (-> FAB)

--- app/src/main/java/com/mobileapplikationenfhws/speiseplan/MainActivity.java
+++ app/src/main/java/com/mobileapplikationenfhws/speiseplan/MainActivity.java
@@ -1,5 +1,6 @@
 package com.mobileapplikationenfhws.speiseplan;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,21 +10,23 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.mobileapplikationen.speiseplan.meallist.MeallistActivity;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, MeallistActivity.class);
+                startActivity(intent);
             }
         });
     }
